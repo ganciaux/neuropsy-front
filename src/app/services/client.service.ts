481da5de4def6bbeb4ca740aebbad8c2669cac2d
ganciaux <ghislain.anciaux@live.fr>
2024-01-25 14:57:24 +0100

    Add environment and HttpClient

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@angular/core';
 import { Client } from '../models/client.model';
 import { CLIENTS } from '../mocks/Clients.mock';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environments';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ClientService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   getClients(): Observable<Client[]> {
-    return of(CLIENTS);
+    //return of(CLIENTS);
+    return this.http.get<Client[]>(`${environment.apiURL}/clients`)
   }
 
   getClient(id: number): Observable<Client | undefined> {
